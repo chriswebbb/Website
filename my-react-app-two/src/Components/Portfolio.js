706cdce401646a342d8a792e6a4daeaf7a23e8dd
Christian Webb <mater.webb99@googlemail.com>
2021-10-14 23:49:13 +0100

    Updated the work panel

--- my-react-app-two/src/Components/Portfolio.js
+++ my-react-app-two/src/Components/Portfolio.js
@@ -6,38 +6,34 @@ class Portfolio extends Component {
     if(this.props.data){
       var projects = this.props.data.projects.map(function(projects){
         var projectImage = 'images/portfolio/'+projects.image;
-        return <div key={projects.title} className="columns portfolio-item">
-           <div className="item-wrap">
+        return <div key={projects.title} className="grid-style portfolio-item">
+          <div className="item-wrap">
             <a href={projects.url} title={projects.title}>
                <img alt={projects.title} src={projectImage} />
                <div className="overlay">
                   <div className="portfolio-item-meta">
-                 <h5>{projects.title}</h5>
+                  <h5>{projects.title}</h5>
                      <p>{projects.category}</p>
                   </div>
                 </div>
               <div className="link-icon"><i className="fa fa-link"></i></div>
+            <p className="defaultText">{projects.description}</p>
             </a>
-          </div>
+          </div>  
         </div>
       })
     }
 
     return (
       <section id="portfolio">
-
-      <div className="row">
-
-         <div className="twelve columns collapsed">
-
+        <div className="rows">
             <h1>Some of My Work</h1>
 
-            <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
+            <div id="portfolio-wrapper" className="grid-container bgrid-quarters s-bgrid-thirds">
                 {projects}
             </div>
-          </div>
-      </div>
-   </section>
+        </div>
+    </section>
     );
   }
 }
